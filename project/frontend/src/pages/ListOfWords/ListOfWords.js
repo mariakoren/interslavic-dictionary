7a38f33b4a27b7c1ae421bfa2e759b7329a8f181
mariakoren <maria.korenn@gmail.com>
2024-06-11 12:34:14 +0200

    feat: styles list of words

--- project/frontend/src/pages/ListOfWords/ListOfWords.js
+++ project/frontend/src/pages/ListOfWords/ListOfWords.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import './listofwords.css';
 
 const ListOfWords = () => {
     const [data, setData] = useState(null);
@@ -9,14 +10,14 @@ const ListOfWords = () => {
             .get("http://localhost:5000/listofwords")
             .then(res => setData(res.data))
             .catch(err => console.error(err))
-    });
+    }, []);
 
     return data ? (
         <>
           {data.map((rec, i) => 
-          <ul>
-            <li>Polski: {rec.polish}</li>
-            <li>Międzysłowiański: {rec.interslavic}</li>
+          <ul key={i} className="word-list">
+            <li className="word-item">Polski: {rec.polish}</li>
+            <li className="word-item">Międzysłowiański: {rec.interslavic}</li>
           </ul>
         )}
         </> ) : (
